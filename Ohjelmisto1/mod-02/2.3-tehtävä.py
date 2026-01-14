7bb830c6aa15b724e3b08c057046ee676cb6e43a
Samu Tolvanen <samu.s.tolvanen@gmail.com>
2026-01-14 11:35:54 +0200

    Lisää tehtäviä

---
+++ Ohjelmisto1/mod-02/2.3-tehtävä.py
@@ -0,0 +1,4 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+pintaala = (kanta*korkeus)
+print("Suorakulmion pinta-ala: "+ str(pintaala))
