faa8aa027f258fe9a83176c14479c11a10ce4a85
Samu Tolvanen <samu.s.tolvanen@gmail.com>
2026-01-19 10:32:49 +0200

    Lisää tehtäviä

---
+++ Ohjelmisto1/mod-03/3.1-tehtävä.py
@@ -0,0 +1,4 @@
+kuha = float(input("Anna kuhan pituus senttimetreissä: "))
+alinpyynti = (37-kuha)
+if kuha<37:
+    print(f"Laske kuha takaisin järveen\nKuhalla puuttuu {alinpyynti:.0f} senttiä pyyntimitasta")
---
+++ Ohjelmisto1/mod-03/3.3-tehtävä.py
@@ -0,0 +1,14 @@
+sukupuoli = input("Anna biologinen sukupuoli: ")
+hemoglobiini = int(input("Anna hemoglobiini arvo (g/l): "))
+
+if (sukupuoli == "mies" or sukupuoli == "Mies") and hemoglobiini < 134:
+    print("Hemoglobiini arvo on alhainen")
+
+elif (sukupuoli == "nainen" or sukupuoli == "Nainen") and hemoglobiini < 117:
+    print("Hemoglobiini arvo on alhainen")
+
+elif (sukupuoli == "nainen" or sukupuoli == "Nainen") and hemoglobiini > 175:
+    print("Hemoglobiini arvo on korkea")
+
+else:
+    print("Hemoglobiini arvo on normaali")
