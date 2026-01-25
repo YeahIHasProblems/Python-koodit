4cf22eb2352ee8cf5b9b267cb568adf1e556b3c1
Samu Tolvanen <samu.s.tolvanen@gmail.com>
2026-01-25 20:05:50 +0200

    Lisää tehtäviä

---
+++ Ohjelmisto1/mod-04/4.6-tehtävä.py
@@ -0,0 +1,15 @@
+import random
+
+pisteet = int(input("Kuinka monta pistettä arvotaan? "))
+numero = 0
+kerrat = 0
+
+while kerrat < pisteet:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x**2 + y**2 < 1:
+        numero = numero + 1
+    kerrat = kerrat + 1
+
+pi = 4 * numero / pisteet
+print(f"Piin likiarvo:{pi}")
